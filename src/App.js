d65779152fcd2f478e24fc3db4f6101725215721
6th <shujitsu@gmail.com>
2020-09-26 20:50:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Tittle</h1>
+        <p>lorem</p>
+      </div>
+      <div className="container">
+        <h1>Tittle</h1>
+        <p>lorem</p>
+      </div>
+      <div className="container">
+        <h1>Tittle</h1>
+        <p>lorem</p>
+      </div>
+      </>
+  );
+}
+
+export default App;
